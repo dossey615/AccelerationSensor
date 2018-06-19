df398124e7786b7bc6b89d684bae3767d8768442
dossey615 <dossey615@gmail.com>
2018-06-19 11:26:11 +0900

    first commit

---
+++ AccelerationSensor/AccelerationInfomation.swift
@@ -0,0 +1,9 @@
+//
+//  AccelerationInfomation.swift
+//  AccelerationSensor
+//
+//  Created by 土居将史 on 2018/06/19.
+//  Copyright © 2018年 土居将史. All rights reserved.
+//
+
+import Foundation
--- AccelerationSensor/ViewController.swift
+++ AccelerationSensor/ViewController.swift
@@ -7,13 +7,35 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
+    
+    //MotionManagerのインスタンス生成
+    let motion: CMMotionManager = CMMotionManager()
 
+    //X,Y,Zの値を表示するTextLabel
+    @IBOutlet weak var valueX: UILabel!
+    @IBOutlet weak var valueY: UILabel!
+    @IBOutlet weak var valueZ: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 更新周期を設定.
+        motion.accelerometerUpdateInterval = 0.1
+        
+        motion.startAccelerometerUpdates(to: OperationQueue.current!, withHandler: {(accelData: CMAccelerometerData?, errorOC:Error?) -> Void in
+            self.valueX.text = String(format: "%06f", (accelData?.acceleration.x)!)
+            self.valueY.text = String(format: "%06f", (accelData?.acceleration.y)!)
+            self.valueZ.text = String(format: "%06f", (accelData?.acceleration.z)!)
+            })
     }
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
